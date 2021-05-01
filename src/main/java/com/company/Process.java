3a0cba6d56bf17fea7285b47420cec9bea5ce7e5
Sanket Bhangale <sanketvbhangale@gmail.com>
2021-05-01 20:33:26 +0200

    Comments

--- src/main/java/com/company/Process.java
+++ src/main/java/com/company/Process.java
@@ -3,5 +3,10 @@ package com.company;
 public interface Process {
     int getId();
     int getPriority();
+
+    /**
+     * The client should implement what it means to kill a process
+     * @throws TerminationException
+     */
     void freeResources() throws TerminationException;
 }
